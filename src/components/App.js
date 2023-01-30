842beb2ea7bd19f05e92895f96f9a0e66ccc2b63
Robin <robin.james.guerrier@gmail.com>
2023-01-30 15:22:38 -0500

    Setup files.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='App' >
+            <header className='header' >
+                <h1 className='header--title' >Phonebook</h1>
+            </header>
+        </div>
+    );
+};
+
+export default App;
